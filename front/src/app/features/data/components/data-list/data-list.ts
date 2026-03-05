84c7fbbf59c6b926e6374703073546dbbf0deb8f
Vincent Danchaud <vdanchaud@gmail.com>
2026-03-05 11:18:16 +0100

    create data-list component in feature-folder

---
+++ front/src/app/features/data/components/data-list/data-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-list',
+  imports: [],
+  templateUrl: './data-list.html',
+  styleUrl: './data-list.css',
+})
+export class DataList {
+
+}
