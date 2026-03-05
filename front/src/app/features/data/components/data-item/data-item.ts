2d82e551e016d748199284800f81cccf291be40c
Vincent Danchaud <vdanchaud@gmail.com>
2026-03-05 11:19:10 +0100

    create data-item component in features/data

---
+++ front/src/app/features/data/components/data-item/data-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-item',
+  imports: [],
+  templateUrl: './data-item.html',
+  styleUrl: './data-item.css',
+})
+export class DataItem {
+
+}
