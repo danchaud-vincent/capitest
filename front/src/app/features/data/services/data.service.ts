17a90bd7f5033c76d9c2a1e5af7271f288d860b2
Vincent Danchaud <vdanchaud@gmail.com>
2026-03-05 11:28:40 +0100

    create data service and add Http client to get data from the backend

--- front/src/app/features/data/services/data.service.ts
+++ front/src/app/features/data/services/data.service.ts
@@ -1,6 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Data } from '../models/data.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class DataService {}
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  getData(): Observable<Data[]> {
+    return this.http.get<Data[]>('http://localhost/capitest/back/items');
+  }
+}
